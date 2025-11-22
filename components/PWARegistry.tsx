6eff48f1d4f8bed72cb91691a0249d32623e34c1
UROUS <urous3814@gmail.com>
2025-11-22 17:31:10 +0900

    feat: conditionally register/unregister PWA service worker based on current path

--- components/PWARegistry.tsx
+++ components/PWARegistry.tsx
@@ -1,24 +1,34 @@
 "use client";
 
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function PWARegistry() {
+  const pathname = usePathname();
+
   useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      "serviceWorker" in navigator &&
-      (window as any).workbox === undefined
-    ) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((registration) => {
-          console.log("Service Worker registered: ", registration);
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed: ", error);
+    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+      if (pathname === "/pwa") {
+        if ((window as any).workbox === undefined) {
+          navigator.serviceWorker
+            .register("/sw.js")
+            .then((registration) => {
+              console.log("Service Worker registered: ", registration);
+            })
+            .catch((error) => {
+              console.error("Service Worker registration failed: ", error);
+            });
+        }
+      } else {
+        navigator.serviceWorker.getRegistrations().then((registrations) => {
+          for (const registration of registrations) {
+            registration.unregister();
+            console.log("Service Worker unregistered");
+          }
         });
+      }
     }
-  }, []);
+  }, [pathname]);
 
   return null;
 }
